ed963fddd05314ce6cb9cff4ccd111a0ff91d1f9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-11 14:01:19 +0300

    Initial Commit

---
+++ RoutingInSwiftUI/RoutingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutingInSwiftUIApp.swift
+//  RoutingInSwiftUI
+//
+//  Created by Ramill Ibragimov on 11.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct RoutingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
