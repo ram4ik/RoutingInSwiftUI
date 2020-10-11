0954820e4205a3d7cd0729452f2490b8fcd71136
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-11 14:25:39 +0300

    init

--- RoutingInSwiftUI/ContentView.swift
+++ RoutingInSwiftUI/ContentView.swift
@@ -7,10 +7,67 @@
 
 import SwiftUI
 
+enum Route {
+    case detail(String)
+    case aboutUs
+}
+
+struct Navigator {
+    static func navigate<T: View>(_ route: Route, content: () -> T) -> AnyView {
+        
+        switch route {
+        case .detail(let animal):
+            return AnyView(NavigationLink(
+                destination: DetailsView(animal: animal)) {
+                content()
+            })
+            
+            
+        case .aboutUs:
+            return AnyView(NavigationLink(
+                destination: AboutUsView()) {
+                content()
+            })
+        }
+    }
+}
+
 struct ContentView: View {
+    let animals = ["Cat", "Mouse", "Dog"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List(animals, id: \.self) { animal in
+                    Navigator.navigate(.detail(animal)) {
+                        Text(animal)
+                    }
+                }.listStyle(PlainListStyle())
+                
+                Button(action: {
+                    
+                }, label: {
+                    Navigator.navigate(.aboutUs) {
+                        Text("Go to About View")
+                    }
+                })
+            }
+        }
+    }
+}
+
+struct DetailsView: View {
+    let animal: String
+    
+    var body: some View {
+        Text(animal)
+            .font(.largeTitle)
+    }
+}
+
+struct AboutUsView: View {
+    var body: some View {
+        Text("About")
     }
 }
 
